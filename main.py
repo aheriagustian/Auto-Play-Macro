699eb23a5d8a71a8dd569c8227dd0d3531e54ccf
selenium_34@naver.com <selenium_34@naver.com>
2021-01-21 22:39:20 +0900

    gui frame

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from tkinter import *
 from tkinter.ttk import *
-from tkinter import ttk
+from tkinter import ttk, StringVar
 from tkinter import messagebox
 
 import time
-import pyautogui
-
 
 # gui 창 생성
 win = Tk()
@@ -22,7 +21,7 @@ lb_school.grid(row=0, column=0)
 cb_sch = ttk.Combobox(win, values=["서울대학교", "연세대학교", "인천대학교", "인하대학교"])
 print(dict(cb_sch))
 cb_sch.grid(row=0, column=1)
-cb_sch.current(1)
+cb_sch.current(3)
 print(cb_sch.current(), cb_sch.get())
 
 # 로그인
@@ -30,18 +29,32 @@ lb_id = Label(win, text=' ID ')
 lb_id.grid(row=2, column=0)
 
 str_id = StringVar()
-tb_id = ttk.Entry(win, width=20, textvariable=str_id)
-tb_id.grid( row = 2, column = 1)
+tb_id = ttk.Entry(win, width=20)
+tb_id.grid(row=2, column=1)
 
 lb_pw = Label(win, text=' PW ')
 lb_pw.grid(row=3, column=0)
 
-str_pw = StringVar()
-tb_pw = ttk.Entry(win, width=20, textvariable=str_pw)
-tb_pw.grid( row = 3, column = 1)
+str_pw: StringVar = StringVar()
+tb_pw = ttk.Entry(win, width=20)
+tb_pw.grid(row=3, column=1)
 
-btn_login = Button(win, text="LOG IN")
+driver = webdriver.Chrome()
+def loginFunc():
+    if cb_sch.get() == "인천대학교":
+        url = 'https://cyber.inu.ac.kr/login.php'
+    elif cb_sch.get() == "인하대학교":
+        url = 'https://learn.inha.ac.kr/login.php'
+    driver.get(url)
+    str_id = tb_id.get()
+    str_pw = tb_pw.get()
+    driver.find_element_by_css_selector('#input-username').send_keys(str_id)  # str_id
+    driver.find_element_by_css_selector('#input-password').send_keys(str_pw)  # str_pw
+    driver.find_element_by_css_selector('#input-password').send_keys(Keys.ENTER)
+
+btn_login = Button(win, text="LOG IN", command=loginFunc)
 btn_login.grid(row=4, column=1)
+
 
 # 강의검색
 lb_lec = Label(win, text=' [ PLAY LIST ]')
@@ -79,18 +92,4 @@ win.mainloop() # 창 실행
 
 
 
-#
-# driver = webdriver.Chrome()+
-# url = 'https://cyber.inu.ac.kr'
-# driver.get(url)
-
-
-
-#
-#
-# # 아이디 비밀번호 자동입력
-# driver.find_element_by_css_selector('#input-username').send_keys('201901739')
-# driver.find_element_by_css_selector('#input-password').send_keys('sh9230915!')
-# # 비번창에서 그냥 엔터
-# driver.find_element_by_css_selector('#input-password').send_keys(Keys.ENTER)
-#
+# div class = "progress_courses"
