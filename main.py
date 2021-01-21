39a0d64416dc2066e5df864e55f9874a6871088a
selenium_34@naver.com <selenium_34@naver.com>
2021-01-21 17:11:18 +0900

    re init

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 url = 'https://cyber.inu.ac.kr'
 driver.get(url)
 
 
-# 아이디 비밀번호 입력
-driver.find_element_by_css_selector('.required.form-control').send_keys('201901739')
-driver.find_element_by_css_selector('.required.form-control').send_keys('201901739')
+# 아이디 비밀번호 자동입력
+driver.find_element_by_css_selector('#input-username').send_keys('201901739')
+driver.find_element_by_css_selector('#input-password').send_keys('sh9230915!')
+# 비번창에서 그냥 엔터
+driver.find_element_by_css_selector('#input-password').send_keys(Keys.ENTER)
+
